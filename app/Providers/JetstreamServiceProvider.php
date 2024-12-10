b1c23a3eeb94a28c8979bb16bbd7b895f59893ba
Willy <willy025896@gmail.com>
2024-12-10 17:39:12 +0800

    feat: 登入頁新增註冊連結

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 use App\Actions\Jetstream\DeleteUser;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Inertia\Inertia;
+use Laravel\Fortify\Fortify;
 use Laravel\Jetstream\Jetstream;
 
 class JetstreamServiceProvider extends ServiceProvider
@@ -24,6 +27,14 @@ class JetstreamServiceProvider extends ServiceProvider
         $this->configurePermissions();
 
         Jetstream::deleteUsersUsing(DeleteUser::class);
+
+        Fortify::loginView(function () {
+            return Inertia::render('Auth/Login', [
+                'canResetPassword' => Route::has('password.request'),
+                'canRegister' => Route::has('register'),
+                'status' => session('status'),
+            ]);
+        });
     }
 
     /**
--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -10,6 +10,7 @@ import TextInput from '@/Components/TextInput.vue';
 
 defineProps({
     canResetPassword: Boolean,
+    canRegister: Boolean,
     status: String,
 });
 
@@ -42,6 +43,15 @@ const submit = () => {
         </div>
 
         <form @submit.prevent="submit">
+            <div class="flex items-center justify-end">
+                <Link
+                    v-if="canRegister"
+                    :href="route('register')"
+                    class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                >
+                    Not registered?
+                </Link>
+            </div>
             <div>
                 <InputLabel for="email" value="Email" />
                 <TextInput
